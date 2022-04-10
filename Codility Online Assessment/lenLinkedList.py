b269c7958538a1e64e65706f7a10031a3a120e62
devchoplife <dritedurojaiye@gmail.com>
2022-04-10 17:35:05 +0100

    Finding Length Of A Linked List

--- Codility Online Assessment/lenLinkedList.py
+++ Codility Online Assessment/lenLinkedList.py
@@ -1,1 +1,38 @@
-# https://www.geeksforgeeks.org/python-program-for-finding-length-of-a-linked-list-iterative-and-recursive-approach/
+"""
+Write a function to count the number of nodes in a given singly linked list
+"""
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def getCount(self):
+        temp = self.head
+        count = 0
+
+        while(temp):
+            count += 1
+            temp = temp.next
+
+        return count
+
+
+llist = LinkedList()
+llist.push(1)
+llist.push(3)
+llist.push(1)
+llist.push(2)
+llist.push(1)
+print("Count of nodes is :", llist.getCount())
