cb57e1447f91734de2b2000ed0db52f5b9696621
devchoplife <dritedurojaiye@gmail.com>
2022-04-10 10:24:56 +0100

    Sign of the Product of an Array

--- Codility Online Assessment/arrayProduct.py
+++ Codility Online Assessment/arrayProduct.py
@@ -1,2 +1,50 @@
-# https://ste176876.medium.com/leetcode-sign-of-the-product-of-an-array-2fb3c8b808ca
-# https://www.geeksforgeeks.org/program-for-product-of-array/
+"""
+There is a function signFunc(x) that returns:
+- 1 if x is positive.
+- -1 if x is negative.
+- 0 if x is equal to 0.
+
+You are given an integer array nums. Let product be the product of all values in the array nums.
+Return signFunc(product).
+
+Example 1:
+Input: nums = [-1,-2,-3,-4,3,2,1]
+Output: 1
+
+Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+
+Example 2:
+Input: nums = [1,5,0,2,-3]
+Output: 0
+
+Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+
+Example 3:
+Input: nums = [-1,1,-1,1,-1]
+Output: -1
+
+Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+
+Constraints:
+- 1 <= nums.length <= 1000
+- -100 <= nums[i] <= 100
+"""
+
+def signFunc(array):
+    arrayProduct = product(array)
+    if arrayProduct == 0:
+        return 0
+    elif (arrayProduct < 0):
+        return -1
+    else:
+        return 1
+
+def product(array):
+    result = 1
+    n = len(array)
+    for i in range(0, n):
+        result = (result * array[i])
+    return result
+
+array = [1, -2, 3, 4, 5]
+print(signFunc(array))
--- README.md
+++ README.md
@@ -22,8 +22,8 @@
 - [Partition array into N subsets with balanced sum](/Codility%20Online%20Assessment/partitionArray.py)
 - [Meeting Rooms II](/Codility%20Online%20Assessment/meetingRooms.py)
 - [Count Good Nodes in a Binary Tree](/Codility%20Online%20Assessment/countBinaryNodes.py)
-
 - [Largest Alphabet Character](/Codility%20Online%20Assessment/largestAlphabet.py)
+
 - [Sign of the Product of an Array](/Codility%20Online%20Assessment/arrayProduct.py)
 - [Equal Subset Sum Partition](/Codility%20Online%20Assessment/partitionArray.py)
 - [Largest number possible after removal of K digits](/Codility%20Online%20Assessment/removeKDigits.py)
