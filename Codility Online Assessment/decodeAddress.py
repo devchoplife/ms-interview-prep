62f5947d5a2a83ce0d394f5e7e2c8b581a364580
Durojaiye <michael.durojaiye@savethechildren.org>
2022-04-11 17:15:40 +0100

    Python program to check if the given string is IPv4 or IPv6 or Invalid

--- Codility Online Assessment/decodeAddress.py
+++ Codility Online Assessment/decodeAddress.py
@@ -1,1 +1,42 @@
-# https://www.geeksforgeeks.org/python-program-to-check-if-the-given-string-is-ipv4-or-ipv6-or-invalid/
+"""
+Given a string. The task is to check if the given string is IPv4 or IPv6 or Invalid.
+
+Examples:
+
+Input : “192.168.0.1”
+Output : IPv4
+Explanation : It is a valid IPv4 address
+
+Input : “2001:0db8:85a3:0000:0000:8a2e:0370:7334”
+Output : IPv6
+Explanation : It is a valid IPv6 address
+
+Input : “255.32.555.5”
+Output : Invalid
+Explanation : It is an invalid IPv4 address as the 3rd octet value(i.e 555) is greater 255.
+
+Input : “250.32:555.5”
+Output : Invalid
+Explanation : The given string is invalid as it consists of both : and .
+"""
+from ipaddress import ip_address, IPv4Address
+
+def validIPAddress(IP: str) -> str:
+    try:
+        return "IPv4" if type(ip_address(IP)) is IPv4Address else "IPv6"
+    except ValueError:
+        return "Invalid"
+
+
+Ip = "192.168.0.1"
+print(validIPAddress(Ip)) 
+  
+Ip = "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
+print(validIPAddress(Ip)) 
+  
+Ip = "256.32.555.5"
+print(validIPAddress(Ip))  
+  
+Ip = "250.32:555.5"
+print(validIPAddress(Ip))
+        
