87f91545f7797af05a374f59571fcb5636f44f1a
Durojaiye <michael.durojaiye@savethechildren.org>
2022-04-11 18:11:16 +0100

    Rotate a Linked List

--- Codility Online Assessment/rotateList.py
+++ Codility Online Assessment/rotateList.py
@@ -1,2 +1,58 @@
-# https://www.geeksforgeeks.org/rotate-a-linked-list/
-# 
+"""
+Given a singly linked list, rotate the linked list counter-clockwise by k nodes. Where k is a given positive 
+integer. For example, if the given linked list is 10->20->30->40->50->60 and k is 4, the list should be 
+modified to 50->60->10->20->30->40. Assume that k is smaller than the count of nodes in a linked list.
+"""
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.nex = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+        
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data), 
+            temp = temp.next
+    
+    def rotate(self, k):
+        if k == 0: return
+        
+        current = self.head
+        count = 1
+        while (count < k and current is not None):
+            current = current.next
+            count += 1
+            
+        if current is None:
+            return 
+        
+        kthNode = current
+        while (current.next is not None):
+            current = current.next
+            
+        current.next = self.head
+        self.head = kthNode.next
+        kthNode.next = None
+
+
+llist = LinkedList()
+ 
+# Create a list 10->20->30->40->50->60
+for i in range(60, 0, -10):
+    llist.push(i)
+ 
+print("Given linked list")
+llist.printList()
+llist.rotate(4)
+ 
+print("\nRotated Linked list")
+llist.printList()
--- README.md
+++ README.md
@@ -37,8 +37,8 @@
 - [Number of flips to make binary string alternate](/Codility%20Online%20Assessment/coinFlip.py)
 - [Python program to check if the given string is IPv4 or IPv6 or Invalid](/Codility%20Online%20Assessment/decodeAddress.py)
 - [Populating Next Right Pointers in Each Node](/Codility%20Online%20Assessment/rightPointers.py)
-
 - [Rotate a Linked List](/Codility%20Online%20Assessment/rotateList.py)
+
 - [Best Time to Buy and Sell Stock](/Codility%20Online%20Assessment/bestStock.py)
 - [Number of ways to cut a stick of length N into K pieces](/Codility%20Online%20Assessment/cutStick.py)
 - [Find minimum amplitude after deleting K elements](/Codility%20Online%20Assessment/amplitude.py)
